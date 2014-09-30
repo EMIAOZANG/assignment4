0f9d88275b3defe2c4d43f712827dcf91f8049e0
Israel Malkin <israelmalkin@nyu.edu>
2014-09-30 17:02:28 -0400

    answers5.py added

---
+++ im965/answer5.py
@@ -0,0 +1,16 @@
+#answer5.py
+#im965 program for assignment 03, question 05
+
+
+#!/usr/bin/env python
+
+#These two expressions look the same, but product different results
+
+a=5*3**2
+print a
+
+b=(5*3)**2
+print b
+
+print "Compare the two:"
+print a , b
