62c3e198501b8958e22e12b01558fdc6b4cb5b3b
Israel Malkin <israelmalkin@nyu.edu>
2014-09-30 15:53:32 -0400

    answers4.py added

---
+++ im965/answer4.py
@@ -0,0 +1,21 @@
+#answer4.py
+#im965 program for assignment 03, question 04
+
+#!/usr/bin/env python
+
+a=(3*5)/(2+3)
+print a
+
+b=((7.0+9)**(0.5))*(2.0)
+print b
+
+c=(4.0-7)**(3.0)
+print c
+
+d=(-19.0+100.0)**(1.0/4.0)
+print d
+
+e=6%4
+print e
+
+
