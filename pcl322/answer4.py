d3635128db65489850269a850bd94e93cab76864
Po-Chih Lin <pcl322@nyu.edu>
2014-10-01 14:38:09 -0400

    First commit

---
+++ pcl322/answer4.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import numpy
+
+if __name__=="__main__":
+
+	print (3*5)/(2+3)
+	print numpy.sqrt(7+9)*2
+	print numpy.power(4-7, 3)
+	print numpy.power(-19+100, 0.25)
+	print 6 % 4
