bb544aafdd5d6b081165234452257d6a98e6c26d
Po-Chih Lin <pcl322@nyu.edu>
2014-10-01 15:10:20 -0400

    Ans 4 final (I think)

--- pcl322/answer4.py
+++ pcl322/answer4.py
@@ -1,15 +1,14 @@
 import sys
 import os
-import numpy
 
 if __name__=="__main__":
 
 	v = [0]*5
 
 	v[0] = (3*5)/(2+3)
-	v[1] = numpy.sqrt(7+9)*2
-	v[2] = numpy.power(4-7, 3)
-	v[3] = numpy.power(-19+100, 0.25)
+	v[1] = (7+9)**0.5*2
+	v[2] = (4-7)**3
+	v[3] = (-19+100)**0.25
 	v[4] = 6 % 4
 
 	for i in range(5):
