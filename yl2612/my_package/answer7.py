9303b4d4f691c4f460adc00e4aa2d7af69597d55
Kayla Li <yl2612@nyu.edu>
2014-10-01 21:56:20 -0400

    edited answer7

--- yl2612/my_package/answer7.py
+++ yl2612/my_package/answer7.py
@@ -5,9 +5,9 @@ Created on Sep 30, 2014
 '''
 def main():
     li=[1,2,3,4,5,6,7,8,9]
-    li=li[0:4]*4
+    li=li[0:4]
+    li.extend([1,1,1,2,2,3,3,3,3,4,4,4])
     li.sort()
-    li[7]=3
     print li
 if __name__ == '__main__':
     main()
