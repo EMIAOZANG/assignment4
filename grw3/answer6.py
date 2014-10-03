6cad455d23a4cfb151529e966df5bd6593a25b97
Greg Watson <g.watson@computer.org>
2014-10-02 23:41:23 -0400

    answers

---
+++ grw3/answer6.py
@@ -0,0 +1,8 @@
+first_name=raw_input('Enter your first name: ')
+last_name=raw_input('Enter your last name: ')
+print 'Enter your date of birth:'
+birth_month=raw_input('Month? ')
+birth_day=input('Day? ')
+birth_year=input('Year? ')
+print '%s %s was born on %s %d, %d.' %(first_name, last_name, birth_month, birth_day, birth_year)
+
